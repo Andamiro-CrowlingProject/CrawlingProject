6e94781cfa84c18c4fae2fae7a76dea90fd59489
sinnybb <153700515+sinnybb@users.noreply.github.com>
2024-06-14 17:28:38 +0900

    including docstring

--- GoogleCrawling.py
+++ GoogleCrawling.py
@@ -25,226 +25,254 @@ import time
 import pandas as pd
 import numpy as np
 
-def setup_crawling():
-    # 크롬 옵션 설정
-    chrome_options = Options()
-    chrome_options.add_argument("--disable-popup-blocking") # 팝업창 차단 설정
-    chrome_options.add_argument("--disable-notifications") # 알림 요청 차단
-    chrome_options.add_argument('--no-sandbox')
-    chrome_options.add_argument("--headless")  # headless 모드 설정
-    chrome_options.add_argument("--window-size=1920x1080")  # headless 모드에서 창 크기 설정
 
-    # ChromeDriver 자동 설치 및 경로 가져오기
-    chrome_driver_path = chromedriver_autoinstaller.install()
+class GoogleCrawling:
+    """
+    Google 이미지를 크롤링하는 클래스입니다.
 
-    # Service 객체 생성
-    service = Service(chrome_driver_path)
+    메서드:
+    setup_crawling():
+        크롬 드라이버를 설정하고 반환합니다.
 
-    # 크롬 드라이버에 옵션 적용
-    driver = webdriver.Chrome(service=service, options=chrome_options)
-    
-    return driver
+    crawl_images(search_query, max_images=10, wait_time=2):
+        주어진 검색어로 이미지를 크롤링하여 URL 리스트를 반환합니다.
 
-def crawl_images(driver, search_query, max_images=10, search_time=0.5, wait_time=2, scroll_pause_time=5):
-    # Google URL
-    url = 'https://www.google.com/'
+    save_images(self, search_query, download_path, url_list, max_images):
+        이미지를 다운로드하고 저장합니다.
 
-    # Driver setting
-    driver.get(url)
+    find_duplicates(self, download_path, similarity_threshold=30):    
+        중복이미지를 검출합니다.
+    """
 
-    # 검색어 입력 및 제출
-    element = driver.find_element(By.NAME, 'q')
-    element.send_keys(search_query)
-    element.submit()
-    time.sleep(search_time)
+    def __init__(self, search_query, max_images, download_path):
+        """
+        GoogleCrawling 클래스의 인스턴스를 초기화합니다.
 
-    # '이미지' 탭으로 이동
-    try:
-        images_tab = WebDriverWait(driver, 10).until(
-            EC.element_to_be_clickable((By.LINK_TEXT, '이미지'))
-        )
-        images_tab.click()
-    except:
+        Args:
+        search_query (str): 검색어.
+        max_images (int): 다운로드할 최대 이미지 수.
+        download_path (str): 다운로드할 경로.
+        """
+        self.search_query = search_query
+        self.max_images = max_images
+        self.download_path = download_path
+        self.url = 'https://www.google.com/'
+
+
+    def setup_crawling(self):
+        """
+        크롬 드라이버를 설정하고 반환합니다.
+        """
+        chrome_options = Options()
+        chrome_options.add_argument("--disable-popup-blocking")
+        chrome_options.add_argument("--disable-notifications")
+        chrome_options.add_argument('--no-sandbox')
+        chrome_options.add_argument("--headless")
+        chrome_options.add_argument("--window-size=1920x1080")
+
+        chrome_driver_path = chromedriver_autoinstaller.install()
+        service = Service(chrome_driver_path)
+        driver = webdriver.Chrome(service=service, options=chrome_options)
+        
+        return driver
+
+
+    def crawl_images(self, search_query, max_images=10, wait_time=2):
+        """
+        주어진 검색어로 이미지를 크롤링하여 URL 리스트를 반환합니다.
+
+        Args:
+        search_query (str): 검색어.
+        max_images (int): 다운로드할 최대 이미지 수. 기본값은 10.
+        wait_time (int): 대기 시간(초). 기본값은 2초.
+        scroll_pause_time (int): 스크롤 후 대기 시간(초). 기본값은 5초.
+        """
+        driver = self.setup_crawling()
+        driver.get(self.url)
+
+        element = driver.find_element(By.NAME, 'q')
+        element.send_keys(search_query)
+        element.submit()
+        time.sleep(wait_time)
+
+        # '이미지' 탭 클릭
         try:
             images_tab = WebDriverWait(driver, 10).until(
-                EC.element_to_be_clickable(By.LINK_TEXT, 'Images')
+                EC.element_to_be_clickable((By.LINK_TEXT, '이미지'))
             )
             images_tab.click()
         except:
-            print("'이미지' 탭을 찾을 수 없습니다.")
-            driver.quit()
-            return []
+            try:
+                images_tab = WebDriverWait(driver, 10).until(
+                    EC.element_to_be_clickable((By.LINK_TEXT, 'Images'))
+                )
+                images_tab.click()
+            except:
+                print("'이미지' 탭을 찾을 수 없습니다.")
+                driver.quit()
+                return []
 
-    time.sleep(wait_time)
+        time.sleep(wait_time)
 
-    # 필요한 변수 초기화
-    images_collected = 0
-    url_list = []
+        images_collected = 0
+        url_list = []
 
-    while images_collected < max_images:
-        try:
-            # 이미지 큰 범위 선택
-            image_area = WebDriverWait(driver, 10).until(
-                EC.presence_of_all_elements_located((By.CSS_SELECTOR, '.H8Rx8c'))
-            )
-            print(image_area)
-            
-            for image_contents in image_area:
-                try:
-                    # 요소가 화면에 나타나도록 스크롤
-                    driver.execute_script("arguments[0].scrollIntoView();", image_contents)
-                    time.sleep(1)  # 잠시 대기
-
-                    # 요소 클릭 시도
-                    image_contents.click()
-                    print(f"Clicked on: {image_contents}")
-
+        while images_collected < max_images:
+            try:
+                image_area = WebDriverWait(driver, 10).until(
+                    EC.presence_of_all_elements_located((By.CSS_SELECTOR, '.H8Rx8c'))
+                )
+                
+                for image_contents in image_area:
                     try:
-                        # src 요소 수집
-                        src_elements = WebDriverWait(driver, 10).until(
-                            EC.presence_of_all_elements_located((By.CSS_SELECTOR, '.sFlh5c.pT0Scc.iPVvYb'))
-                        )
-                        print('Read the src')
+                        driver.execute_script("arguments[0].scrollIntoView();", image_contents)
+                        time.sleep(1)
+                        image_contents.click()
 
-                        # href 요소 수집
-                        atag_href_elements = WebDriverWait(driver, 10).until(
-                            EC.presence_of_all_elements_located((By.CSS_SELECTOR, 'a.umNKYc'))
-                        )
-                        if atag_href_elements:
-                            href_element = atag_href_elements[0].get_attribute('href')
-                            print(f"href: {href_element}")
-                        else:
-                            href_element = None
+                        try:
+                            # 이미지 src 수집
+                            src_elements = WebDriverWait(driver, 10).until(
+                                EC.presence_of_all_elements_located((By.CSS_SELECTOR, '.sFlh5c.pT0Scc.iPVvYb'))
+                            )
+                            
+                            # 이미지 href 수집
+                            atag_href_elements = WebDriverWait(driver, 10).until(
+                                EC.presence_of_all_elements_located((By.CSS_SELECTOR, 'a.umNKYc'))
+                            )
+                            href_element = atag_href_elements[0].get_attribute('href') if atag_href_elements else None
 
-                        # 각 src 요소의 src 속성 값 / href 요소의 href 속성 값을 추출하여 리스트에 추가
-                        for src_element in src_elements:
-                            src_value = src_element.get_attribute('src')
-                            print(f"src: {src_value}")
+                            # src, href 매핑
+                            for src_element in src_elements:
+                                src_value = src_element.get_attribute('src')
+                                url_list.append((src_value, href_element))
 
-                            url_list.append((src_value, href_element))
+                            images_collected += len(src_elements)
+                            if images_collected >= max_images:
+                                break
 
-                        images_collected += len(src_elements)
-                        if images_collected >= max_images:
-                            break
+                        except TimeoutException as e:
+                            print("[ Error type ] Timeout while waiting for elements")
+                            print(e)
+                            continue
 
-                    except TimeoutException as e:
-                        print("[ Error type ] Timeout while waiting for elements")
+                    except ElementClickInterceptedException as e:
+                        print("[ Error type ] ElementClickInterceptedException")
                         print(e)
-                        continue  # 다음 이미지 탭으로 이동
+                        continue
 
-                except ElementClickInterceptedException as e:
-                    print("[ Error type ] ElementClickInterceptedException")
-                    print(e)
-                    continue  # 다음 이미지 탭으로 이동
+            except Exception as e:
+                print("[ Error type ] General Exception")
+                print(e)
 
-        except Exception as e:
-            print("[ Error type ] General Exception")
-            print(e)
-            traceback.print_exc()
-
-    # 브라우저 종료
-    driver.quit()
-    return url_list
-
-def save_images(search_query, download_path, url_list, max_images):
-    # CrawlingImage 폴더 경로 설정
-    crawling_image_path = os.path.join(download_path, 'CrawlingImage')
-
-    # CrawlingImage 폴더가 없으면 생성
-    if not os.path.exists(crawling_image_path):
-        os.makedirs(crawling_image_path)
-
-    # search_query 폴더 경로 설정
-    download_path = os.path.join(crawling_image_path, search_query)
-
-    # search_query 폴더가 없으면 생성, 있으면 모든 파일 삭제
-    if not os.path.exists(download_path):
-        os.makedirs(download_path)
-    else:
-        print('기존의 파일 삭제 후 작업 시행합니다.')
-        for file_name in os.listdir(download_path):
-            file_path = os.path.join(download_path, file_name)
-            if os.path.isfile(file_path):
-                os.remove(file_path)
-
-    # 이미지 다운로드 및 저장
-    count = 1
-    for imgUrl, siteUrl in url_list:
-        try:
-            img_data = requests.get(imgUrl).content
-
-            # URL에서 확장자 추출
-            _, ext = os.path.splitext(imgUrl)
-
-            # 확장자가 유효하지 않거나 없는 경우 기본적으로 .jpg로 설정
-            if ext.lower() not in ['.jpg', '.jpeg', '.png', '.gif', '.svg', '.webp']:
-                ext = '.jpg'
-
-            # 이미지 저장 경로 설정
-            image_path = os.path.join(download_path, f"{search_query}_{count}{ext}")
-
-            with open(image_path, 'wb') as handler:
-                handler.write(img_data)
-            print(f"Image saved: {search_query}_{count}{ext}")
-            count += 1
-            if count > max_images:
-                break
-        except Exception as e:
-            print(f"Failed to save image {count}: {e}")
-
-    # URL 리스트를 CSV 파일로 저장
-    image_num = [f'{search_query}_{num}' for num in range(1, count)]
-    url_df = pd.DataFrame({
-        'image_name': image_num,
-        'imgUrl': [url[0] for url in url_list[:count-1]],
-        'siteUrl': [url[1] for url in url_list[:count-1]]
-    })
-    url_df.to_csv(os.path.join(download_path, 'image_url.csv'), index=False)
-    print("CSV file saved: image_url.csv")
+        driver.quit()
+        return url_list
 
 
-def find_duplicates(download_path, similarity_threshold=30):
-    print("중복 이미지 검출 중입니다.")
-    
-    # 이미지 파일 읽기
-    images = [file for file in os.listdir(download_path) if file.lower().endswith(('.jpg', '.jpeg', '.png', '.gif', '.svg', '.webp'))]
-    image_paths = [os.path.join(download_path, img) for img in images]
-
-    # ORB 디텍터 생성
-    orb = cv2.ORB_create()
-
-    # 이미지별로 특징점과 기술자 추출
-    descriptors = []
-    for image_path in image_paths:
-        img = cv2.imread(image_path, cv2.IMREAD_GRAYSCALE)
-        if img is None:
-            print(f"이미지를 읽을 수 없습니다: {image_path}")
-            continue
-        _, des = orb.detectAndCompute(img, None)
-        descriptors.append((image_path, des))
-
-    # 모든 이미지 쌍에 대해 유사도 계산
-    duplicates = []
-    for (img1, des1), (img2, des2) in combinations(descriptors, 2):
-        if des1 is None or des2 is None:
-            continue
+    def save_images(self, search_query, download_path, url_list, max_images):
+        """
+        이미지를 다운로드하고 저장합니다.
         
-        # BFMatcher : 전수 조사 매칭(객체 인식&추적)
-        bf = cv2.BFMatcher(cv2.NORM_HAMMING, crossCheck=True)
-        matches = bf.match(des1, des2)
+        Args:
+            search_query (str): 검색 쿼리.
+            download_path (str): 이미지를 저장할 경로.
+            url_list (list): 이미지 URL 및 사이트 URL 리스트.
+            max_images (int): 다운로드할 최대 이미지 수.
+        """
+        # search_query 폴더가 없으면 생성, 있으면 모든 파일 삭제
+        if not os.path.exists(download_path):
+            os.makedirs(download_path)
+        else:
+            print('기존의 파일 삭제 후 작업 시행합니다.')
+            for file_name in os.listdir(download_path):
+                file_path = os.path.join(download_path, file_name)
+                if os.path.isfile(file_path):
+                    os.remove(file_path)
+
+        # 이미지 다운로드 및 저장
+        count = 1
+        for imgUrl, siteUrl in url_list:
+            try:
+                img_data = requests.get(imgUrl).content
+
+                # URL에서 확장자 추출
+                _, ext = os.path.splitext(imgUrl)
+
+                # 확장자가 유효하지 않거나 없는 경우 기본적으로 .jpg로 설정
+                if ext.lower() not in ['.jpg', '.jpeg', '.png', '.gif', '.svg', '.webp']:
+                    ext = '.jpg'
+
+                # 이미지 저장 경로 설정
+                image_path = os.path.join(download_path, f"{search_query}_{count}{ext}")
+
+                with open(image_path, 'wb') as handler:
+                    handler.write(img_data)
+                print(f"Image saved: {search_query}_{count}{ext}")
+                count += 1
+                if count > max_images:
+                    break
+            except requests.exceptions.RequestException as e:
+                print(f"Failed to save image {count}: {e}")
+
+        # URL 리스트를 CSV 파일로 저장
+        image_num = [f'{search_query}_{num}' for num in range(1, count)]
+        url_df = pd.DataFrame({
+            'image_name': image_num,
+            'imgUrl': [url[0] for url in url_list[:count-1]],
+            'siteUrl': [url[1] for url in url_list[:count-1]]
+        })
+        url_df.to_csv(os.path.join(download_path, 'image_url.csv'), index=False)
+        print("CSV file saved: image_url.csv")
+
+
+    def find_duplicates(self, download_path, similarity_threshold=30):
+        """
+        중복 이미지를 검출합니다.
         
-        # 유사도는 매칭된 특징점의 수로 결정
-        similarity = len(matches)
+        Args:
+            download_path (str): 이미지를 저장한 경로.
+            similarity_threshold (int): 유사도 임계값.
+        """
+        print("중복 이미지 검출 중입니다.")
+        
+        # 이미지 파일 읽기
+        images = [file for file in os.listdir(download_path) if file.lower().endswith(('.jpg', '.jpeg', '.png', '.gif', '.svg', '.webp'))]
+        image_paths = [os.path.join(download_path, img) for img in images]
 
-        # 유사도가 특정 임계값 이상이면 중복으로 간주
-        if similarity > similarity_threshold:  
-            duplicates.append((img1, img2))
+        # ORB 디텍터 생성
+        orb = cv2.ORB_create()
 
-    if duplicates:
-        for dup in duplicates:
-            print(f"중복된 이미지: {dup[0]} and {dup[1]}")
-    else:
-        print("중복된 이미지가 없습니다.")
+        # 이미지별로 특징점과 기술자 추출
+        descriptors = []
+        for image_path in image_paths:
+            img = cv2.imread(image_path, cv2.IMREAD_GRAYSCALE)
+            if img is None:
+                # print(f"이미지를 읽을 수 없습니다: {image_path}")
+                continue
+            _, des = orb.detectAndCompute(img, None)
+            descriptors.append((image_path, des))
+
+        # 모든 이미지 쌍에 대해 유사도 계산
+        duplicates = []
+        for (img1, des1), (img2, des2) in combinations(descriptors, 2):
+            if des1 is None or des2 is None:
+                continue
+            
+            # BFMatcher : 전수 조사 매칭(객체 인식&추적)
+            bf = cv2.BFMatcher(cv2.NORM_HAMMING, crossCheck=True)
+            matches = bf.match(des1, des2)
+            
+            # 유사도는 매칭된 특징점의 수로 결정
+            similarity = len(matches)
+
+            # 유사도가 특정 임계값 이상이면 중복으로 간주
+            if similarity > similarity_threshold:  
+                duplicates.append((img1, img2))
+
+        if duplicates:
+            for dup in duplicates:
+                print(f"중복된 이미지: {dup[0]} and {dup[1]}")
+        else:
+            print("중복된 이미지가 없습니다.")
 
 
 if __name__ == "__main__":
@@ -255,18 +283,15 @@ if __name__ == "__main__":
     path = input('저장 경로를 설정해주세요 :') + '\\CrawlingImage\\'
     download_path = path + f'{search_query}'
 
-    # 드라이버 셋업
-    driver = setup_crawling()
-
+    # 크롤링
     try:
-        # 크롤링
-        url_list = crawl_images(driver, search_query, max_images)
-        
-        # 이미지 저장
-        save_images(search_query, download_path, url_list, max_images)
+        GC = GoogleCrawling(search_query, max_images, download_path)
 
-        # 중복찾기
-        find_duplicates(download_path)
+        url_list = GC.crawl_images(search_query, max_images) # 크롤링
+        GC.save_images(search_query, download_path, url_list, max_images) # 이미지 저장
+        GC.find_duplicates(download_path) # 중복찾기
 
     except Exception as e:
         print("An error occurred:", e)
+
+    
